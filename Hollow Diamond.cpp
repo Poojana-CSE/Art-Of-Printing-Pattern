2b19c1d0e0870d80cf89f3c3323413a6298f124c
08021976-22022006 <poojanas.dev@gmail.com>
2025-02-07 15:04:26 +0530

    Hollow Pattern

---
+++ Hollow Diamond.cpp
@@ -0,0 +1,37 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0;i<2*n-1;i++){
+//         for(int j=0;j<2*n-1;j++){
+//             if(i+j==n-1 || i==j+n-1 || i==j-n+1 || i+j==3*n-3)
+//                 cout<<"*";
+//             else 
+//                 cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// i+j==n-1
+//         *
+//     *
+// *
+
+
+// i==j+n-1
+// *
+//     *
+//         *
+
+// i==j-n+1
+//         *
+//             *
+//                 *
+
+// i+j==3*n-3
+
+//                     *
+//                 *
+//             *
